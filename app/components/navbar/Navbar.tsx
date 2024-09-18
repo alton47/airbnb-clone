3b67ecfe6557533c407d9a4c948be34ede2fa379
Allan <altonallan47@gmail.com>
2024-09-18 17:12:38 +0200

    Add AirBnB logo

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,8 +1,16 @@
+import Container from "../Container"
+import Logo from "./Logo"
 
 const Navbar = () => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
-      <div className="py-4 border-b-[1px]"></div>
+      <div className="py-4 border-b-[1px]">
+        <Container>
+            <div className="flex flex-row items-center justsify-between gap-3 mg:gap-0">
+                <Logo />
+            </div>
+        </Container>
+      </div>
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}> 
-        <Navbar/>
+        <Navbar />
         {children} 
       </body>
     </html>
