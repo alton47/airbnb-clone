3873fd4ba9845c7a80d9b5e77044f09fdffb752a
Allan <altonallan47@gmail.com>
2024-09-18 16:58:43 +0200

    Create navbar.tsx

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,8 +1,8 @@
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]"></div>
     </div>
   )
 }
