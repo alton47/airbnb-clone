91da486bcb6bda0ee3ffeb6f93007e26e6708acd
Allan <altonallan47@gmail.com>
2024-09-19 22:36:46 +0200

    Create Modals

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
+import Modal from "./components/modals/Modal";
 
 export const metadata: Metadata = {
   title: "AirBnB clone | Allan Alton",
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}> 
+        <Modal />
         <Navbar />
         {children} 
       </body>
