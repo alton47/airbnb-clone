22f506142aeb58e9125afa58c78833e296cc6129
Allan <altonallan47@gmail.com>
2024-09-19 22:16:47 +0200

    Update UserMenu
    Fix callback import error

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,9 +1,17 @@
 'use client';
 
+import { useState,useCallback } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 
 const UserMenu = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleOpen = useCallback(() => {
+        setIsOpen((value) => !value);
+    }, []);
+
+
   return (
     <div className="realtive">
       <div className="flex flex-row items-center gap-3">
@@ -11,7 +19,7 @@ const UserMenu = () => {
             Airbnb your home
         </div>
         <div
-          onClick={() => {}}
+          onClick={toggleOpen}
           className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center transition gap-3 rounded-full cursor-pointer hover:shadow-md">
             <AiOutlineMenu />
             <div className="hidden md:block">
@@ -19,6 +27,14 @@ const UserMenu = () => {
             </div>
         </div>
       </div>
+
+      {isOpen && (
+        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-o top-12 text-sm">
+            <div className="flex flex-col cursor-pointer">
+                
+            </div>
+        </div>
+      )}
     </div>
   )
 }
