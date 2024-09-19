cbbc1a1343bc49b2c5856cb15f8e9d45a743b8d8
Allan <altonallan47@gmail.com>
2024-09-19 21:46:49 +0200

    Update usermenu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,8 +1,20 @@
+'use client';
+
+import { AiOutlineMenu } from "react-icons/ai";
 
 const UserMenu = () => {
   return (
-    <div>
-      User
+    <div className="realtive">
+      <div className="flex flex-row items-center gap-3">
+        <div className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
+            Airbnb your home
+        </div>
+        <div
+          onClick={() => {}}
+          className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center transition gap-3 rounded-full cursor-pointer hover:shadow-md">
+            <AiOutlineMenu />
+        </div>
+      </div>
     </div>
   )
 }
