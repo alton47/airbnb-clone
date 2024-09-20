d78e98b248cdc4e0bc5c06e342d38ab86d2e2044
Allan <altonallan47@gmail.com>
2024-09-20 16:32:19 +0200

    Update Modal

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -65,9 +65,9 @@ const Modal: React.FC<ModalProps> = ({
     }
 
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70"></div>
+    </>
   )
 }
 
