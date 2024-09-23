9e81c2f7b2657924babfc579aa0a86bab5c683ea
Allan <altonallan47@gmail.com>
2024-09-23 22:51:56 +0200

    Add Footer to Modal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -10,6 +10,7 @@ import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/Input';
 import toast from 'react-hot-toast';
+import Button from '../Button';
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
@@ -76,6 +77,35 @@ const RegisterModal = () => {
                required 
             />
         </div>
+    );
+
+    const footerContent = (
+        <div className='flex flex-col gap-4 mt-3'>
+            <hr />
+            <Button 
+               outline
+               label='Continue with Google'
+               icon={FcGoogle}
+               onClick={() => {}}
+            />
+             <Button 
+               outline
+               label='Continue with GitHub'
+               icon={AiFillGithub}
+               onClick={() => {}}
+            />
+            <div className='text-neutral-500 text-center mt-4 font-light'>
+                <div className='text-center flex flex-row items-center gap-2'>
+                    <div>
+                        Already have an Account?
+                    </div>
+                    <div className='text-neutral-800 cursor-pointer hover:underline'
+                         onClick={registerModal.onClose}>
+                        Log In
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 
 
@@ -88,6 +118,7 @@ const RegisterModal = () => {
        onClose={registerModal.onClose}
        onSubmit={handleSubmit(onSubmit)}
        body={bodyContent}
+       footer={footerContent}
     />
   )
 }
