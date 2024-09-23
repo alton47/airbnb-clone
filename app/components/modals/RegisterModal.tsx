8ad45542172e155e6c0838cf0d06f23688c3db10
Allan <altonallan47@gmail.com>
2024-09-23 21:14:40 +0200

    Update Input

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -8,6 +8,7 @@ import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
 import Heading from '../Heading';
+import Input from '../inputs/Input';
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
@@ -48,6 +49,7 @@ const RegisterModal = () => {
                title='Welcome to Faza'
                subtitle='Create an Account!'
             />
+            <Input />
         </div>
     )
 
