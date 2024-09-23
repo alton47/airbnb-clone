5772031c403453aea4360b47d9e1eed65826d08d
Allan <altonallan47@gmail.com>
2024-09-23 20:52:08 +0200

    Create Heading Component

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -7,6 +7,7 @@ import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
+import Heading from '../Heading';
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
@@ -41,6 +42,12 @@ const RegisterModal = () => {
         })
     }
 
+    const bodyContent = (
+        <div className='flex flex-col gap-4'>
+            <Heading />
+        </div>
+    )
+
 
   return (
     <Modal 
@@ -50,6 +57,7 @@ const RegisterModal = () => {
        actionLabel='Continue'
        onClose={registerModal.onClose}
        onSubmit={handleSubmit(onSubmit)}
+       body={bodyContent}
     />
   )
 }
