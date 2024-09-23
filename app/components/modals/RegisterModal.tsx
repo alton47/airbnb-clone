1d1906e612c9e2bb3c6e3c7947dbc979044733a8
Allan <altonallan47@gmail.com>
2024-09-23 17:56:15 +0200

    Update RegisterModal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,6 +1,47 @@
-import React from 'react'
+'use client';
+
+import axios from 'axios';
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
 
 const RegisterModal = () => {
+    const registerModal = useRegisterModal();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const {
+        register,
+        handleSubmit,
+        formState: {
+            errors,
+        }
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: ''
+        }
+    });
+
+    const onSubmit : SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true);
+
+        axios.post('/api/register', data)
+        .then(() => {
+            registerModal.onClose();
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+        .finally(() => {
+            setIsLoading(false);
+        })
+    }
+
+// Tumeishia hapaaa 1:10:17
+
   return (
     <div>
       
